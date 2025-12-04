a9093d825bc699d464d325429aee2af82ccc1e13
sunpubio <sunpubio@gmail.com>
2025-12-04 15:39:27 +0900

    modify the mistake

--- R/test.R
+++ R/test.R
@@ -22,7 +22,7 @@ fixarea_6800 <- function(df, matched_area) {
         (100 * 10000 * matched_area * (1000 - CorrFact / H2O_s)),
       Ci = -((gtc - E / 2) * Ca - A) / (gtc + E / 2),
       PPFD = Qin,
-      gtw = E * (1000-((1000*0.61365*(exp(17.502*TleafCnd/(240.97+TleafCnd))/(Pa+ΔPcham)))+H2O)/2/(1000*0.61365(exp(17.502*TleafCnd/(240.97*TleafCnd))/(Pa+ΔPcham))-H2O)),
+      gtw = E * (1000-((1000*0.61365*(exp(17.502*TleafCnd/(240.97+TleafCnd))/(Pa+ΔPcham)))+H2O_s)/2/(1000*0.61365*(exp(17.502*TleafCnd/(240.97*TleafCnd))/(Pa+ΔPcham))-H2O_s)),
       gsw = 2/((1/gtw-1/gbw)+sign(gtw)*sqrt((1/gtw-1/gbw)*(1/gtw-1/gbw)+4*K/((K+1)^2*(2*1/gtw*1/gbw-(1/gbw)^2)))),
       gtc = 1/((((K+1)/(gsw/1.6))+1/(gbw/1.37))+(K/((K+1/(gsw/1.6))+(K/(gbw/1.37))))),
       hhmmss = hhmmss
