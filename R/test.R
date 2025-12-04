d126de1096244dc3d56769fa1ce214cefaae46ee
sunpubio <sunpubio@gmail.com>
2025-12-04 14:56:32 +0900

    add gtw, gsw, gtc

--- R/test.R
+++ R/test.R
@@ -10,7 +10,7 @@ fixarea_6800 <- function(df, matched_area) {
   df |>
     dplyr::select(
       obs, A, Ci, CO2_r, CO2_s, Tleaf, E, gtc, Ca, S,
-      H2O_r, H2O_s, CorrFact, Flow, Qin
+      H2O_r, H2O_s, CorrFact, Flow, Qin, TleafCnd, K, Pa, ΔPacham, gbw
     ) |>
     dplyr::mutate(
       matched_area = matched_area,
@@ -22,8 +22,9 @@ fixarea_6800 <- function(df, matched_area) {
         (100 * 10000 * matched_area * (1000 - CorrFact / H2O_s)),
       Ci = -((gtc - E / 2) * Ca - A) / (gtc + E / 2),
       PPFD = Qin,
-      gsc = A / (Ca - Ci),
-      gsw = 1.6 * gsc,
+      gtw = E * (1000-((1000*0.61365*(exp(17.502*TleafCnd/(240.97+TleafCnd))/(Pa+ΔPcham)))+H2O)/2/(1000*0.61365(exp(17.502*TleafCnd/(240.97*TleafCnd))/(Pa+ΔPcham))-H2O)),
+      gsw = 2/((1/gtw-1/gbw)+sign(gtw)*sqrt((1/gtw-1/gbw)*(1/gtw-1/gbw)+4*K/((K+1)^2*(2*1/gtw*1/gbw-(1/gbw)^2)))),
+      gtc = 1/((((K+1)/(gsw/1.6))+1/(gbw/1.37))+(K/((K+1/(gsw/1.6))+(K/(gbw/1.37))))),
       hhmmss = hhmmss
     )
 }
