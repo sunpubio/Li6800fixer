1199c193b96610c122f11b5586be404272993f32
sunpubio <sunpubio@gmail.com>
2025-12-04 16:30:19 +0900

    change all code

--- R/test.R
+++ R/test.R
@@ -20,15 +20,39 @@ fixarea_6800 <- function(df, matched_area) {
         CO2_r - CO2_s * ((1000 - CorrFact * H2O_r) /
                            (1000 - CorrFact * H2O_s))
       ) / (100 * 10000 * matched_area),
+
+      # ここは従来どおり（E は mmol ベースでOK）
       E = 1000 * CorrFact * Flow * (H2O_s - H2O_r) /
         (100 * 10000 * matched_area * (1000 - CorrFact / H2O_s)),
+
       Ci = -((gtc - E / 2) * Ca - A) / (gtc + E / 2),
       PPFD = Qin,
-      gtw = E * (1000-((1000*0.61365*(exp(17.502*TleafCnd/(240.97+TleafCnd))/(Pa+ΔPcham)))+H2O_s)/2/(1000*0.61365*(exp(17.502*TleafCnd/(240.97+TleafCnd))/(Pa+ΔPcham))-H2O_s)),
-      gsw = 2/((1/gtw-1/gbw)+sign(gtw)*sqrt((1/gtw-1/gbw)*(1/gtw-1/gbw)+4*K/((K+1)^2*(2*1/gtw*1/gbw-(1/gbw)^2)))),
-      gtc = 1/((((K+1)/(gsw/1.6))+1/(gbw/1.37))+(K/((K+1/(gsw/1.6))+(K/(gbw/1.37))))),
+
+      # ★ gtw は E を mol に直して使う（E_mol = E / 1000）
+      E_mol = E / 1000,
+
+      gtw = E_mol * (
+        1000 - ((1000 * 0.61365 * exp(17.502 * TleafCnd / (240.97 + TleafCnd)) /
+                   (Pa + ΔPcham)) + H2O_s) / 2
+      ) / (
+        1000 * 0.61365 * exp(17.502 * TleafCnd / (240.97 + TleafCnd)) /
+          (Pa + ΔPcham) - H2O_s
+      ),
+
+      gsw = 2 / (
+        (1 / gtw - 1 / gbw) +
+          sign(gtw) * sqrt(
+            (1 / gtw - 1 / gbw)^2 +
+              4 * K / ((K + 1)^2 * (2 * 1 / gtw * 1 / gbw - (1 / gbw)^2))
+          )
+      ),
+
+      gtc = 1 / (
+        (((K + 1) / (gsw / 1.6)) + 1 / (gbw / 1.37)) +
+          (K / ((K + 1 / (gsw / 1.6)) + (K / (gbw / 1.37))))
+      ),
+
       hhmmss = hhmmss
-    )
-
+    ) |>
+    dplyr::select(-E_mol)   # 中間列は消しておく
 }
-
